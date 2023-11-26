4b7e86161baf2ab4aaee9c8040502750366fc1af
Havriil Pietukhin <pietukhin1@uniba.sk>
2023-11-26 14:10:03 +0100

    [REQ-28 REQ-29 REQ-31 REQ-35] files from Spring Initialzr

---
+++ src/main/kotlin/com/example/rvtool/RvtoolApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rvtool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RvtoolApplication
+
+fun main(args: Array<String>) {
+    runApplication<RvtoolApplication>(*args)
+}
