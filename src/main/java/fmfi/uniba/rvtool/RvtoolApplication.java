72ccc2296950984a818d240dd7a55b73772ab0fb
Havriil Pietukhin <pietukhin1@uniba.sk>
2023-11-30 21:39:13 +0100

    [REQ-28 REQ-29 REQ-31 REQ-35] files from Spring Initialzr

---
+++ src/main/java/fmfi/uniba/rvtool/RvtoolApplication.java
@@ -0,0 +1,13 @@
+package fmfi.uniba.rvtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvtoolApplication.class, args);
+    }
+
+}
