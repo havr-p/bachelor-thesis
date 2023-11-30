462e670f37854dd6512d06e8df6db53817eac583
havriilp <havriil.pietukhin@consultis.sk>
2023-11-30 21:39:13 +0100

    [REQ-28 REQ-29 REQ-31 REQ-35] files from Spring Initialzr

---
+++ src/test/java/fmfi/uniba/rvtool/RvtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package fmfi.uniba.rvtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
