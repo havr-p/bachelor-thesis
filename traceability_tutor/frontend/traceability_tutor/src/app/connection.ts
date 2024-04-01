04c3de4e1b3dda3c44c4fdc84a6e04330aaaa792
Havriil Pietukhin <pietukhin1@uniba.sk>
2024-04-01 23:27:02 +0200

    implement custom connection

--- traceability_tutor/frontend/traceability_tutor/src/app/connection.ts
+++ traceability_tutor/frontend/traceability_tutor/src/app/connection.ts
@@ -1,10 +1,24 @@
-import { ClassicPreset } from "rete";
-import { NodeProps } from "./types";
+import { ClassicPreset } from 'rete';
+import { NodeProps } from './types';
+import { Subject } from 'rxjs';
+
+// export class Connection<
+//   A extends NodeProps,
+//   B extends NodeProps
+// > extends ClassicPreset.Connection<A, B> {
+//   //isLoop?: boolean;
+//   label?: string;
+// }
 
 export class Connection<
   A extends NodeProps,
-  B extends NodeProps
+  B extends NodeProps,
 > extends ClassicPreset.Connection<A, B> {
-  //isLoop?: boolean;
-  label?: string;
+  isSelected?: boolean;
+  private changes = new Subject<any>();
+  changes$ = this.changes.asObservable();
+  updateData(data: any) {
+    this.isSelected = data.isSelected;
+    this.changes.next(data);
+  }
 }
