a6d9b1c668113fdee103c89250b9c320a81f3b52
havriilp <havriil.pietukhin@consultis.sk>
2024-04-02 13:03:32 +0200

    added new source node type, added event type

--- traceability_tutor/frontend/traceability_tutor/src/app/types.ts
+++ traceability_tutor/frontend/traceability_tutor/src/app/types.ts
@@ -1,15 +1,28 @@
-import { GetSchemes } from 'rete';
+import { ClassicPreset, GetSchemes } from 'rete';
 import { Connection } from './connection';
 import { RequirementNode } from './nodes/requirement.node';
+import { SourceNode } from './nodes/SourceNode';
 
 export type NodeProps =
   // | DebugChat
   // | Message
   // | OnMessage
   // | MatchMessage
-  RequirementNode;
+  RequirementNode | SourceNode;
 export type ConnProps = Connection<RequirementNode, RequirementNode>;
 // | Connection<MatchMessage, SendMessage>
 // | Connection<OnMessage, MatchMessage>;
 
 export type Schemes = GetSchemes<NodeProps, ConnProps>;
+
+export enum Events {
+  ADD = 'add',
+  REMOVE = 'remove',
+  EDIT = 'edit',
+  SELECT = 'select',
+  LOAD = 'load',
+  DEMO = 'demo',
+  EXPORT = 'export',
+  IMPORT = 'import',
+  CLEAR = 'clear',
+}
