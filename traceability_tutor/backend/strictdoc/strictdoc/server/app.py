db52ff96cc1c5e8db5ccd178f44d5d779496a75a
havriilp <havriil.pietukhin@consultis.sk>
2024-04-02 16:31:51 +0200

    added origin for preview builds

--- traceability_tutor/backend/strictdoc/strictdoc/server/app.py
+++ traceability_tutor/backend/strictdoc/strictdoc/server/app.py
@@ -26,7 +26,7 @@ def create_app(
         "http://localhost:3000",
         "http://localhost:4200",
             "https://bachelor-thesis-alpha.vercel.app",
-            "https://**.vercel.app",
+            "https://traceability-tutor-git-python001-havr-ps-projects.vercel.app",
     ]
 
     # Uncomment this to enable performance measurements.
