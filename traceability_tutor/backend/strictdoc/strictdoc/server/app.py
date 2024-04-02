7f38854688177bbf1cad4668626e6e2b29d8c6cf
Havriil Pietukhin <pietukhin1@uniba.sk>
2024-04-02 16:09:51 +0200

    fix(server): add vercel url to allowed origins

--- traceability_tutor/backend/strictdoc/strictdoc/server/app.py
+++ traceability_tutor/backend/strictdoc/strictdoc/server/app.py
@@ -24,7 +24,9 @@ def create_app(
         "http://localhost",
         "http://localhost:8081",
         "http://localhost:3000",
-        "http://localhost:4200"
+        "http://localhost:4200",
+        "https://bachelor-thesis-alpha.vercel.app",
+        "https://traceability-tutor-git-python001-havr-ps-projects.vercel.app",
     ]
 
     # Uncomment this to enable performance measurements.
