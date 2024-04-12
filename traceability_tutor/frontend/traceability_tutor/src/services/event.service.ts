f1893f6b3c76999994610643484b30add0c4613c
havriilp <havriil.pietukhin@consultis.sk>
2024-04-12 10:45:33 +0200

    add event type

--- traceability_tutor/frontend/traceability_tutor/src/services/event.service.ts
+++ traceability_tutor/frontend/traceability_tutor/src/services/event.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Subject} from "rxjs";
+import { Subject } from 'rxjs';
+import { EditorEvent } from '../app/types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventService {
-private eventSource = new Subject<any>();
+  private eventSource = new Subject<any>();
   event$ = this.eventSource.asObservable();
 
-  publishEditorEvent(data: any) {
-    this.eventSource.next(data);
+  publishEditorEvent(type: EditorEvent, data: any) {
+    console.log('publishEditorEvent', type, data);
+    this.eventSource.next({ type, data });
   }
 }
