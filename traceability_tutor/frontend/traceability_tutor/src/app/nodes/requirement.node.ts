5772ce990fa56eed3cba6759c1d087c545fa208b
havriilp <havriil.pietukhin@consultis.sk>
2024-04-12 10:47:16 +0200

    pass data into interface

--- traceability_tutor/frontend/traceability_tutor/src/app/nodes/requirement.node.ts
+++ traceability_tutor/frontend/traceability_tutor/src/app/nodes/requirement.node.ts
@@ -1,41 +1,28 @@
-import {ClassicPreset, ClassicPreset as Classic, GetSchemes, NodeEditor} from 'rete';
-import {Component, HostBinding, Injector, Input} from '@angular/core';
-import { Area2D, AreaExtensions, AreaPlugin } from 'rete-area-plugin';
+import { Requirement } from '../models/requirement';
+import { getColorByLevel } from '../utils';
+import { Node } from './Node';
+import { ClassicPreset } from 'rete';
+import { NodeType } from '../types';
 
-import {
-  AngularPlugin,
-  AngularArea2D,
-  Presets as AngularPresets,
-} from 'rete-angular-plugin/17';
-
-import { DataflowEngine, DataflowNode } from 'rete-engine';
-import { structures } from 'rete-structures'
-import { ChangeDetectorRef} from "@angular/core";
-import {Requirement} from "../models/requirement";
-import {getColorByLevel} from "../utils";
-
-export class RequirementNode extends ClassicPreset.Node {
-   width = 400;
-   height = 200;
-   backgroundColor: string;
+export class RequirementNode extends ClassicPreset.Node implements Node {
+  width = 400;
+  height = 200;
+  type = NodeType.REQUIREMENT;
+  backgroundColor: string;
   borderStyle: string;
-   requirement: Requirement;
-   constructor(requirement: Requirement) {
-     super(requirement.name);
-     //console.log(requirement);
-     this.requirement = requirement;
-     this.id = requirement.id;
-     this.borderStyle = '2px solid #000000';
-     this.backgroundColor = getColorByLevel(requirement.level);
-     this.selected = false;
+  data: any;
+  constructor(requirement: Requirement) {
+    super(requirement.name);
+    //console.log(requirement);
+    this.id = requirement.id;
+    this.borderStyle = '2px solid #000000';
+    this.backgroundColor = getColorByLevel(requirement.level);
+    this.selected = false;
+    this.data = requirement;
   }
 
-
- // execute() {}
- //  data() {
- //    return this.requirement;
- //  }
-
-
+  // execute() {}
+  //  data() {
+  //    return this.requirement;
+  //  }
 }
-
