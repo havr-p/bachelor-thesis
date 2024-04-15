a41a50277b75f6f55cea5e7c038b844acd925423
Havriil Pietukhin <pietukhin1@uniba.sk>
2024-04-15 22:24:41 +0200

    feat(editor): allow user to select node

--- traceability_tutor/frontend/traceability_tutor/src/app/ui/traceability-editor/traceability-editor.component.ts
+++ traceability_tutor/frontend/traceability_tutor/src/app/ui/traceability-editor/traceability-editor.component.ts
@@ -2,11 +2,10 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  ViewChild,
-  OnInit,
+  Injector,
   OnDestroy,
-  Output,
-  EventEmitter,
+  OnInit,
+  ViewChild,
 } from '@angular/core';
 
 // import {ClassicPreset, ClassicPreset as Classic, GetSchemes, NodeEditor} from 'rete';
@@ -17,215 +16,14 @@ import {
 //   AngularArea2D,
 //   Presets as AngularPresets,
 // } from 'rete-angular-plugin/17';
-
-import { DataflowEngine, DataflowNode } from 'rete-engine';
 import {
   AutoArrangePlugin,
   Presets as ArrangePresets,
 } from 'rete-auto-arrange-plugin';
-import { ReadonlyPlugin } from 'rete-readonly-plugin';
-import {
-  ContextMenuPlugin,
-  ContextMenuExtra,
-  Presets as ContextMenuPresets,
-} from 'rete-context-menu-plugin';
+import { ContextMenuExtra, ContextMenuPlugin } from 'rete-context-menu-plugin';
 import { MinimapExtra, MinimapPlugin } from 'rete-minimap-plugin';
-// import {addCustomBackground} from "../../customization/custom-background";
-// import {RequirementNode} from "../../nodes/requirement.node";
-// import {CustomConnectionComponent} from "../../customization/custom-connection/custom-connection.component";
-// import {CustomSocketComponent} from "../../customization/custom-socket/custom-socket.component";
-// import {EventService} from "../../../services/event.service";
-// import {Requirement} from "../../models/requirement";
-// import {RequirementNodeComponent} from "../requirement-node/requirement-node.component";
-// import {
-//   ConnectionPlugin,
-//   Presets as ConnectionPresets
-// } from "rete-connection-plugin";
-// import { Schemes } from 'src/app/types';
-// import {TextSocket} from "../../sockets";
-// import {CustomNodeComponent} from "../../customization/custom-node/custom-node.component";
 
-// class NumberNode extends Classic.Node implements DataflowNode {
-//   width = 180;
-//   height = 120;
-//
-//   constructor(initial: number, change?: (value: number) => void) {
-//     super('Number');
-//
-//     this.addOutput('value', new Classic.Output(socket, 'Number'));
-//     this.addControl(
-//       'value',
-//       new Classic.InputControl('number', { initial, change })
-//     );
-//   }
-//   data() {
-//     const value = (this.controls['value'] as Classic.InputControl<'number'>)
-//       .value;
-//
-//     return {
-//       value,
-//     };
-//   }
-// }
-//
-// class AddNode extends Classic.Node implements DataflowNode {
-//   width = 180;
-//   height = 195;
-//
-//   constructor() {
-//     super('Add');
-//
-//     this.addInput('a', new Classic.Input(socket, 'A'));
-//     this.addInput('b', new Classic.Input(socket, 'B'));
-//     this.addOutput('value', new Classic.Output(socket, 'Number'));
-//     this.addControl(
-//       'result',
-//       new Classic.InputControl('number', { initial: 0, readonly: true })
-//     );
-//   }
-//   data(inputs: { a?: number[]; b?: number[] }) {
-//     const { a = [], b = [] } = inputs;
-//     const sum = (a[0] || 0) + (b[0] || 0);
-//
-//     (this.controls['result'] as Classic.InputControl<'number'>).setValue(sum);
-//
-//     return {
-//       value: sum,
-//     };
-//   }
-// }
-//
-// type AreaExtra =
-//   | Area2D<Schemes>
-//   | AngularArea2D<Schemes>
-//   | ContextMenuExtra
-//   | MinimapExtra;
-//
-// const socket = new Classic.Socket('socket');
-//
-// export async function createEditor(container: HTMLElement, injector: Injector) {
-//   const editor = new NodeEditor<Schemes>();
-//   const area = new AreaPlugin<Schemes, AreaExtra>(container);
-//
-//   const angularRender = new AngularPlugin<Schemes, AreaExtra>({ injector });
-//
-//   const readonly = new ReadonlyPlugin<Schemes>();
-//   const contextMenu = new ContextMenuPlugin<Schemes>({
-//     items: ContextMenuPresets.classic.setup([
-//      // ['Number', () => new NumberNode(1, process)],
-//      // ['Add', () => new AddNode()],
-//     ]),
-//   });
-//   const minimap = new MinimapPlugin<Schemes>();
-//   const connection = new ConnectionPlugin<Schemes, AreaExtra>();
-//
-//   editor.use(readonly.root);
-//   addCustomBackground(area);
-//   editor.use(area);
-//   area.use(readonly.area);
-//
-//   area.use(angularRender);
-//
-//   area.use(contextMenu);
-//   area.use(minimap);
-//   area.area.setZoomHandler(null);
-//
-//   angularRender.addPreset(
-//     AngularPresets.classic.setup({
-//       customize: {
-//         node() {
-//           return CustomNodeComponent;
-//         },
-//         connection() {
-//           return CustomConnectionComponent;
-//         },
-//         socket() {
-//           return CustomSocketComponent;
-//         },
-//       },
-//     })
-//   );
-//
-//   angularRender.addPreset(AngularPresets.contextMenu.setup());
-//   angularRender.addPreset(AngularPresets.minimap.setup());
-//   connection.addPreset(ConnectionPresets.classic.setup());
-//
-//   const dataflow = new DataflowEngine<Schemes>();
-//
-//   editor.use(dataflow);
-//
-//    //const a = new RequirementNode(process);
-//   // const b = new RequirementNode(process);
-//    //const add = new AddNode();
-//
-//   // await editor.addNode(a);
-//   // await editor.addNode(b);
-//    //await editor.addNode(add);
-//
-//   // await editor.addConnection(new Connection(a, 'value', add, 'a'));
-//  //  await editor.addConnection(new Connection(b, 'value', add, 'b'));
-//
-//   const arrange = new AutoArrangePlugin<Schemes>();
-//
-//   arrange.addPreset(ArrangePresets.classic.setup());
-//   //arrange.addPreset(ArrangePresets.classic.setup());
-//
-//   area.use(arrange);
-//
-//   await arrange.layout();
-//
-//   AreaExtensions.zoomAt(area, editor.getNodes());
-//
-//
-//   AreaExtensions.simpleNodesOrder(area);
-//
-//   const selector = AreaExtensions.selector();
-//   const accumulating = AreaExtensions.accumulateOnCtrl();
-//
-//   AreaExtensions.selectableNodes(area, selector, { accumulating });
-//
-//   async function process() {
-//     dataflow.reset();
-//
-//     editor
-//       .getNodes()
-//       .filter((node) => node instanceof RequirementNode)
-//       .forEach(async (node) => {
-//         const info = await dataflow.fetch(node.id);
-//
-//         console.log(node.id, 'contains', info);
-//
-//         // area.update(
-//         //   'control',
-//         //   (node.controls['result'] as Classic.InputControl<'number'>).id
-//         // );
-//       });
-//   }
-//
-//   editor.addPipe((context) => {
-//     if (
-//       context.type === 'connectioncreated' ||
-//       context.type === 'connectionremoved'
-//     ) {
-//       process();
-//     }
-//     return context;
-//   });
-//
-//   process();
-//
-// //  readonly.enable();
-//
-//   return {
-//     destroy: () => area.destroy(),
-//     editor: editor,
-//     area: area,
-//     arrange: arrange,
-//   };
-// }
-
-import { NodeEditor, GetSchemes, ClassicPreset } from 'rete';
-import { Injector } from '@angular/core';
+import { ClassicPreset, GetSchemes, NodeEditor } from 'rete';
 import { Area2D, AreaExtensions, AreaPlugin } from 'rete-area-plugin';
 import {
   ConnectionPlugin,
@@ -233,8 +31,8 @@ import {
 } from 'rete-connection-plugin';
 
 import {
-  AngularPlugin,
   AngularArea2D,
+  AngularPlugin,
   Presets as AngularPresets,
 } from 'rete-angular-plugin/17';
 
@@ -244,12 +42,12 @@ import { CustomConnectionComponent } from '../../customization/custom-connection
 import { addCustomBackground } from '../../customization/custom-background';
 import { EventService } from '../../../services/event.service';
 import { Requirement } from '../../models/requirement';
-import { RequirementNodeComponent } from '../../customization/requirement-node/requirement-node.component';
+import { RequirementNodeComponent } from '../nodes/requirement-node/requirement-node.component';
 import { RequirementNode } from '../../nodes/requirement.node';
-import { Events, NodeProps } from '../../types';
+import { EditorEvent, NodeProps } from '../../types';
 import { structures } from 'rete-structures';
-import { Subject } from 'rxjs';
 import { Connection } from '../../connection';
+import { MenuItem } from 'primeng/api';
 
 type Schemes = GetSchemes<
   NodeProps,
@@ -464,11 +262,16 @@ export class TraceabilityEditorComponent
     this.eventService.event$.subscribe(
       async (event: { type: string; data: any }) => {
         switch (event.type) {
-          case Events.DEMO:
+          case EditorEvent.DEMO:
             await this.processDemoEvent(event.data);
             break;
-          case Events.ADD:
+          case EditorEvent.ADD:
             await this.addNode(new RequirementNode(event.data));
+            break;
+          case EditorEvent.SELECT:
+            console.log('selected', event.data);
+            this.openedItem = event.data;
+            this.sidebarVisible = true;
             break;
         }
       },
@@ -527,6 +330,12 @@ export class TraceabilityEditorComponent
     node.addOutput(node.id, new ClassicPreset.Output(socket));
     node.addInput(node.id, new ClassicPreset.Input(socket));
     await this.editor.addNode(node);
+  }
+
+  get openedNodeTitle(): string {
+    console.log('openedItem', this.openedItem);
+    if (this.openedItem) return this.openedItem.requirement.name;
+    return 'item not defined';
   }
 
   ngOnDestroy(): void {
