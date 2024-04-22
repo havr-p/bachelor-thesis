b6c1c96b6282b2e0e6c35b06363fe0fb763ff8ae
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-04-22 10:18:38 +0200

    inject http client into requirements.service.ts

--- traceability_tutor/frontend/traceability_tutor/src/app/services/requirements/requirements.service.ts
+++ traceability_tutor/frontend/traceability_tutor/src/app/services/requirements/requirements.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Requirement } from '../../models/requirement';
 import { HttpResponse } from 'msw';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RequirementsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
   async fetchRequirements(): Promise<Requirement[]> {
     const response = await fetch(`${environment.apiUrl}/requirements/all`);
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -18,6 +20,7 @@ export class RequirementsService {
 
   updateRequirement(requirement: any) {
     console.log('i will save', requirement);
+    //this.http.post();
     return new HttpResponse(null, {
       status: 200,
       statusText: 'ok',
