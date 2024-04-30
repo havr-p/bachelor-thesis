956b88b91a60d86dc157f4ba349702286c35b738
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-04-30 20:42:38 +0200

    add AngularConfig to backend, enable client-server communication

---
+++ traceability-tutor/src/main/java/uniba/fmph/traceability_tutor/config/AngularConfig.java
@@ -0,0 +1,25 @@
+package uniba.fmph.traceability_tutor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class AngularConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*")
+                        .allowedOrigins("http://localhost:4200");
+            }
+
+        };
+    }
+
+}
