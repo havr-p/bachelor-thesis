98c06997f6d749865c28bdff18f71363bfeaf673
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-04-30 20:40:40 +0200

    add Spring Boot backend

---
+++ traceability-tutor/src/main/java/uniba/fmph/traceability_tutor/TraceabilityTutorApplication.java
@@ -0,0 +1,14 @@
+package uniba.fmph.traceability_tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TraceabilityTutorApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TraceabilityTutorApplication.class, args);
+    }
+
+}
