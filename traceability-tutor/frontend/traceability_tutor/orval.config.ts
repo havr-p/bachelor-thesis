6d1b713081b914b92e67d6d8b4bcf79890ba2b3e
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-04-30 23:45:38 +0200

    add DTO generator for client

---
+++ traceability-tutor/frontend/traceability_tutor/orval.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  evo: {
+    output: {
+      mode: "tags",
+      schemas: "gen/model",
+      target: "gen/services",
+      client: 'angular',
+      mock: false, // enable/disable test mock generation
+      // I recommend enabling this option if you generate an api client
+      prettier: false, // recommended if you use prettier
+      clean: true, // recreate the whole folder (avoid outdated files)
+      override: {
+        useDates: true,
+        useNativeEnums: true,
+      },
+    },
+    input: {
+      target: "http://localhost:8080/v3/api-docs",
+    },
+  },
+
+});
