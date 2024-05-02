99da567386f616e7a091841285eb88b76176d3fd
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-02 23:09:22 +0200

    technical commit: add Dockerfile

--- traceability-tutor/frontend/traceability_tutor/src/app/items/Item.ts
+++
@@ -1,8 +0,0 @@
-import { ItemDataType, ItemType } from '../types';
-
-export interface Item {
-  width: number;
-  height: number;
-  type: ItemType | undefined;
-  data: ItemDataType;
-}
--- traceability-tutor/frontend/traceability_tutor/src/app/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, KeyValuePipe } from '@angular/common';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, KeyValuePipe],
-})
-export class SharedModule {}
