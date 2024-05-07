e15d732577d243593bafaca054d3995443ef1665
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-07 23:48:54 +0200

    use default component for 404 error

--- frontend/traceability_tutor/src/app/app-routing.module.ts
+++ frontend/traceability_tutor/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {EditorComponent} from "./ui/editor/editor.component";
 import {AuthComponent} from "./ui/auth/auth.component";
 import {EditorWrapperComponent} from "./ui/editor-wrapper/editor-wrapper.component";
+import {PageNotFoundComponent} from "./ui/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'editor', component: EditorWrapperComponent },
+  { path: 'editor', component: EditorWrapperComponent},
   { path: '', redirectTo: '/auth', pathMatch: 'full' }, //login redirect by default
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
---
+++ frontend/traceability_tutor/src/app/ui/page-not-found/page-not-found.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <!DOCTYPE html>
+    <html lang="en">
+
+
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Traceability Tutor</title>
+    </head>
+
+
+    <body>
+    <div class="d-flex align-items-center justify-content-center vh-100">
+      <div class="text-center">
+        <h1 class="display-1 fw-bold">404</h1>
+        <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+        <p class="lead">
+          The page you’re looking for doesn’t exist.
+        </p>
+        <a [routerLink]="'/editor'" class="btn btn-primary">Go to editor</a>
+      </div>
+    </div>
+    </body>
+
+
+    </html>
+  `,
+  styles: ``
+})
+export class PageNotFoundComponent {
+
+}
