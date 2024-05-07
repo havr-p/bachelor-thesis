4da7fa51ae60ee119cfd050ec7dd17f3ce67eb4a
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-07 23:47:45 +0200

    start using interceptors

--- frontend/traceability_tutor/src/app/app.module.ts
+++ frontend/traceability_tutor/src/app/app.module.ts
@@ -1,36 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CustomSocketComponent } from './customization/custom-socket/custom-socket.component';
+import {AppComponent} from './app.component';
+import {CustomSocketComponent} from './customization/custom-socket/custom-socket.component';
 
-import { CustomConnectionComponent } from './customization/custom-connection/custom-connection.component';
+import {CustomConnectionComponent} from './customization/custom-connection/custom-connection.component';
 
-import { DockComponent } from './ui/dock/dock.component';
-import { MenubarModule } from 'primeng/menubar';
-import { DialogModule } from 'primeng/dialog';
-import { FileUploadModule } from 'primeng/fileupload';
-import { FormsModule } from '@angular/forms';
-import { KeyValuePipe, NgForOf, NgOptimizedImage } from '@angular/common';
-import { RequirementItemComponent } from './ui/items/requirement-item/requirement-item.component';
-import { PanelModule } from 'primeng/panel';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarModule } from 'primeng/sidebar';
-import { SpeedDialModule } from 'primeng/speeddial';
-import { ItemInfoViewComponent } from './ui/item-info-view/item-info-view.component';
-import { SharedModule } from './shared/shared.module';
-import { ReteModule } from 'rete-angular-plugin/17';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { ToastrModule } from 'ngx-toastr';
+import {DockComponent} from './ui/dock/dock.component';
+import {MenubarModule} from 'primeng/menubar';
+import {DialogModule} from 'primeng/dialog';
+import {FileUploadModule} from 'primeng/fileupload';
+import {FormsModule} from '@angular/forms';
+import {KeyValuePipe, NgForOf, NgOptimizedImage} from '@angular/common';
+import {RequirementItemComponent} from './ui/items/requirement-item/requirement-item.component';
+import {PanelModule} from 'primeng/panel';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SidebarModule} from 'primeng/sidebar';
+import {SpeedDialModule} from 'primeng/speeddial';
+import {ItemInfoViewComponent} from './ui/item-info-view/item-info-view.component';
+import {SharedModule} from './shared/shared.module';
+import {ReteModule} from 'rete-angular-plugin/17';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {ToastrModule} from 'ngx-toastr';
 import {AuthComponent} from "./ui/auth/auth.component";
-import { LoginFormComponent } from './ui/login-form/login-form.component';
+import {LoginFormComponent} from './ui/login-form/login-form.component';
 import {TabViewModule} from "primeng/tabview";
 import {InputTextModule} from "primeng/inputtext";
 import {EditorComponent} from "./ui/editor/editor.component";
 import {CreateProjectFormComponent} from "./ui/forms/create-project.form/create-project.form.component";
-import { EditorWrapperComponent } from './ui/editor-wrapper/editor-wrapper.component';
+import {EditorWrapperComponent} from './ui/editor-wrapper/editor-wrapper.component';
 import {provideRouter} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
+import {PageNotFoundComponent} from './ui/page-not-found/page-not-found.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AuthInterceptor} from "./interceptors/auth.interceptor";
+import {APIInterceptor} from "./interceptors/api-interceptor";
 
 @NgModule({
   declarations: [
@@ -41,6 +45,7 @@ import {AppRoutingModule} from "./app-routing.module";
     EditorComponent,
     RequirementItemComponent,
     EditorWrapperComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -68,9 +73,20 @@ import {AppRoutingModule} from "./app-routing.module";
     CreateProjectFormComponent,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
-  exports: [
-  ]
+  exports: []
 })
-export class AppModule {}
+export class AppModule {
+}
