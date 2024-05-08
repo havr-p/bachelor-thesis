51d025199b9638d26ab027dbd39349d5e29a4327
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-09 00:33:27 +0200

    refactor: add dock manager

---
+++ frontend/traceability_tutor/src/app/ui/dock/dock-manager.ts
@@ -0,0 +1,112 @@
+import {RequirementsService} from "../../services/requirements/requirements.service";
+import {EventService} from "../../services/event/event.service";
+import {LocalStorageService} from "../../services/local-storage/local-storage.service";
+import {StateManager} from "../../models/state";
+import {MenuItem} from "primeng/api";
+import {EditorEventType} from "../../types";
+import {Requirement} from "../../models/requirement";
+import {Injectable} from "@angular/core";
+import {ProjectDTO} from "../../../../gen/model";
+import {ProjectResourceService} from "../../../../gen/services/project-resource";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DockManager  {
+  constructor(
+    private requirementsService: RequirementsService,
+    private eventService: EventService,
+    private localStorageService: LocalStorageService,
+    private stateManager: StateManager,
+    private projectService: ProjectResourceService
+  ) {
+  }
+
+  /**
+   * Build a standard menu based on the current mode.
+   * @param mode Mode of the application (e.g., 'projects', 'releases', 'editor')
+   * @returns An array of `MenuItem` suitable for PrimeNG Menubar.
+   */
+  buildMenuItems(mode: 'projects' | 'releases' | 'editor'): MenuItem[] {
+    let items: MenuItem[] = [];
+
+    if (mode === 'editor') {
+      items = [
+        {
+          label: 'Project',
+          items: [
+            {
+              label: 'New...',
+              command: () => {
+                console.log('Creating a new project...');
+              },
+            },
+          ],
+        },
+        {
+          label: 'Load demo project',
+          command: async () => {
+            const data = await this.loadAll();
+            this.eventService.publishEditorEvent(EditorEventType.DEMO, data);
+          },
+        },
+        {
+          label: 'Add node',
+          styleClass: 'menuItem',
+          items: [
+            {
+              label: 'Requirement',
+              tooltip: 'Use this command to create a new requirement node',
+              tooltipPosition: 'bottom',
+              command: async () => {
+                const data: Requirement = {
+                  id: '1',
+                  level: 'stakeholder',
+                  name: 'New Requirement',
+                  statement: 'New Requirement Description',
+                };
+                this.eventService.publishEditorEvent(EditorEventType.ADD, data);
+              },
+            },
+          ],
+        },
+        {
+          label: 'Clear',
+          command: () => {
+            this.localStorageService.clearData();
+            this.eventService.publishEditorEvent(EditorEventType.CLEAR);
+          },
+        },
+      ];
+    } else if (mode === 'projects') {
+      console.log("projects mode")
+      items = this.buildProjectMenuItems();
+    }
+    return items;
+  }
+
+  private async loadAll(): Promise<Requirement[]> {
+    try {
+      return await this.requirementsService.fetchRequirements();
+    } catch (error) {
+      console.error('Error fetching requirements:', error);
+      return [];
+    }
+  }
+
+  private buildProjectMenuItems(): MenuItem[] {
+    let projectMenuItems: MenuItem[] = [];
+
+    projectMenuItems.push(
+      {
+        label: 'New Project',
+        command: () => {
+          console.log('Creating a new project...');
+        }
+      },
+    );
+
+    console.log("projectItems", projectMenuItems)
+    return projectMenuItems;
+  }
+}
