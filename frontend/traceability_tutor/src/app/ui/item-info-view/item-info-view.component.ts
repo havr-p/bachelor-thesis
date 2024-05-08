02fd13bcfb2266c4bdb33b0e697807ad0e348bd5
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-08 16:46:38 +0200

    change generated code path

--- frontend/traceability_tutor/src/app/ui/item-info-view/item-info-view.component.ts
+++ frontend/traceability_tutor/src/app/ui/item-info-view/item-info-view.component.ts
@@ -19,7 +19,7 @@ import { EventService } from 'src/app/services/event/event.service';
 import { ValidationService } from '../../services/validation/validation.service';
 import { RequirementsService } from '../../services/requirements/requirements.service';
 import { Requirement } from '../../models/requirement';
-import {RelationshipDTO} from "../../../../gen/model";
+import {RelationshipDTO} from "../../../../api/model";
 
 @Component({
   selector: 'app-item-info-view',
