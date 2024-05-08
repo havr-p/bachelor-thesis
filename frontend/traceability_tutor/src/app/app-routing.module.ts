72cf786005a93b6954303a86d176c484dfc83790
Havriil Pietukhin <havriil.pietukhin@gmail.com>
2024-05-08 16:44:26 +0200

    add new routes to navigation

--- frontend/traceability_tutor/src/app/app-routing.module.ts
+++ frontend/traceability_tutor/src/app/app-routing.module.ts
@@ -4,10 +4,17 @@ import {EditorComponent} from "./ui/editor/editor.component";
 import {AuthComponent} from "./ui/auth/auth.component";
 import {EditorWrapperComponent} from "./ui/editor-wrapper/editor-wrapper.component";
 import {PageNotFoundComponent} from "./ui/page-not-found/page-not-found.component";
+import {ProjectMenuComponent} from "./ui/project-menu/project-menu.component";
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'editor', component: EditorWrapperComponent},
+  {
+    path: 'editor/:projectId', component: EditorComponent,
+  },
+  {
+    path: 'editor/:projectId/release/:releaseId', component: EditorComponent,
+  },
+  { path: 'projects', component: ProjectMenuComponent },
   { path: '', redirectTo: '/auth', pathMatch: 'full' }, //login redirect by default
   { path: '**', component: PageNotFoundComponent }
 ];
