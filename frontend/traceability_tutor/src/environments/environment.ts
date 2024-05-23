766fbbc481628408745fedb80944aaa033cf67bd
havriilp <havriil.pietukhin@consultis.sk>
2024-05-23 11:07:04 +0200

    fix: change prod api url

--- frontend/traceability_tutor/src/environments/environment.ts
+++ frontend/traceability_tutor/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://traceability-tutor.onrender.com',
+  apiUrl: 'https://traceability-tutor-1.onrender.com',
   oauthRedirectUri: "http://localhost:4200/oauth2/redirect",
 
 };
